d0f5f971b14acdcee9ff00a11f02c22e6b24cce1
Morgan421a <morgan.knight421a@gmail.com>
2026-01-12 09:42:31 +0000

    Added overall event handler using event delegation and cleaned up code

--- backend/todolist.js
+++ backend/todolist.js
@@ -1,57 +1,68 @@
-const buttons = document.getElementsByClassName("checkbox");
+// Checkbox
 let boxTicked = false;
-
-for (let btn of buttons) {
-    btn.addEventListener("click", boxTicker);
-}
 
 function boxTicker(event) {
     boxChecker();
-    tickBox(event.currentTarget);
-}
-    
-    
-function boxChecker() {
-    boxTicked = boxTicked === true ? false : true;
-    console.log(boxTicked);
 }
 
-function tickBox(button) {
-    if (boxTicked == true){
-        button.innerHTML = "✓";
-    } else {
-        button.innerHTML = "";
+function boxChecker() {
+    boxTicked = boxTicked === true ? false : true;
+    if (boxTicked == true) {
+        document.getElementsByClassName("list-item");
+        
     }
 }
 
-const newEntryButton = document.getElementsByClassName("new-entry");
+
+
+
+// New Entry Button
 const list = document.getElementsByTagName("ul")[0];
+const listEntries = [...document.getElementsByTagName("li")];
 
-for (let newBtn of newEntryButton) {
-    newBtn.addEventListener("click", newEntry);
-}
-
-
-let checkbox = document.createElement("input"); //creates a node and specifies the type
+let checkbox = document.createElement("input");
 checkbox.type = "checkbox";
 checkbox.classList.add("checkbox");
-checkbox.addEventListener("click", boxTicker)
 
 function newEntry() {
-    // create new list entry and ask for user input, ensure new entry has a working tickbox
-    let newItem = document.createElement('li');
+    let newItem = document.createElement("li");
     newItem.textContent = prompt("Please enter a task", "New Task");
     newItem.prepend(checkbox);
     list.appendChild(newItem);
+    listEntries.push(newItem);
 }
 
+
+// Delete Button
 const delButton = document.getElementsByClassName("delete");
 
-for (let delBtn of delButton) (
-    delBtn.addEventListener("click", deleteEntry)
-)
-
-
 function deleteEntry() {
-
+    const clickedButton = delButton.closest("li");
+    const index = listEntries().indexOf(delButton);
+    console.log(index);
 }
+
+
+// EVENT DELEGATION
+document.addEventListener("click", e => {
+
+    const deleteBtn = e.target.closest(".delete");
+    if (deleteBtn) {
+        deleteEntry();
+        return;
+    }
+
+    const newEntryBtn = e.target.closest(".new-entry");
+    if (newEntryBtn) {
+        newEntry();
+        return;
+    }
+
+    const chkBox = e.target.closest(".checkbox");
+    if (chkBox) {
+        boxTicker();
+        return;
+    }
+
+    // event delegation, does nothing if click isn't relevant
+});
